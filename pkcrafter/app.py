af84f888c13096848bba0a216b50e07c769c4327
blakeaw <blakeaw1102@gmail.com>
2024-07-03 23:26:49 -0500

    Fully working version (no units yet).

--- pkcrafter/app.py
+++ pkcrafter/app.py
@@ -9,29 +9,33 @@ compartment_counts = {"one": 1, "two": 2, "three": 3}
 col1, col2, col3 = st.columns(3)
 with col2:
     st.header("PK Crafter App")
-with col3:
-    st.image('https://avatars.githubusercontent.com/u/163594810?s=200&v=4', width=100)
+# with col3:
+#     st.image("https://avatars.githubusercontent.com/u/163594810?s=200&v=4", width=100)
+st.logo(
+    "https://avatars.githubusercontent.com/u/163594810?s=200&v=4",
+    link="https://github.com/Borealis-BioModeling",
+)
 st.text(
     "Build and export compartmental PK (pharmacokinetics) models in the PySB format."
 )
-st.write('Powered by PySB ',version("pysb"), " and pysb-pkpd ", version("pysb.pkpd"))
+
 st.write(" ")
 st.markdown("------")
 
-st.header("1. Name The Model")
+st.markdown("### 1. Name The Model")
 model_name = st.text_input("Model Name:", "pkmodel")
 
 st.write(" ")
 st.markdown("------")
 
-st.header("2. Define the Compartments")
+st.markdown("### 2. Define the Compartments")
 
 compartments = []
 n_comp = 1
-#num_compartment = st.radio("Number of Compartments:", ["one", "two", "three", "more"])
-#if num_compartment == "more":
+# num_compartment = st.radio("Number of Compartments:", ["one", "two", "three", "more"])
+# if num_compartment == "more":
 n_comp = st.number_input("Number of Compartments:", value=1, min_value=1, step=1)
-#else:
+# else:
 #    n_comp = compartment_counts[num_compartment]
 
 for i in range(n_comp):
@@ -53,17 +57,23 @@ for i in range(n_comp):
 st.write(" ")
 st.markdown("------")
 
-st.header("3. Define the Drug & Dose")
+st.markdown("### 3. Define the Drug & Dose")
+
+
+dosing = st.radio("Dosing:", list(writing.dosing_strategies.keys()), horizontal=True)
 
 left, right = st.columns(2)
-
-
 with left:
     drug_name = st.text_input("Drug Name: ", "Imagiprofen")
-    drug_dose = st.number_input("Dose:", 0.0)
 
 with right:
-    dosing = st.radio("Dosing:", list(writing.dosing_strategies.keys()))
+    if dosing == "I.V. Infusion":
+        drug_dose = st.number_input(
+            "Dose:", 0.0, help="Dose Infusion Rate (amount/time)"
+        )
+    else:
+        drug_dose = st.number_input("Dose:", 0.0, help="Dose (amount)")
+
     dosing_kwargs = dict()
     if dosing == "Oral":
         dosing_kwargs["ka"] = st.number_input(
@@ -78,24 +88,134 @@ with right:
 st.write(" ")
 st.markdown("------")
 
-st.write(compartments)
+st.markdown("### 4. Drug Distribution")
 
+
+distributes = []
+n_comp = len(compartments)
+if n_comp > 1:
+    st.write(drug_name, " distribution between compartments:")
+    left, center, right = st.columns(3)
+    for i in range(n_comp - 1):
+        comp_i = compartments[i]["name"]
+        for j in range(i + 1, n_comp):
+            comp_j = compartments[j]["name"]
+            left.write(" ")
+            is_on = left.toggle("{} <--> {}".format(comp_i, comp_j))
+            if is_on:
+                k_dist = center.number_input(
+                    "Distribution Rate constant {} --> {}:".format(comp_i, comp_j),
+                    0.0,
+                    help="1st-order rate constant for the distribution.",
+                )
+                # center.latex(r"k_f")
+                k_redist = right.number_input(
+                    "Re-distribution Rate constant {} <-- {}:".format(comp_i, comp_j),
+                    0.0,
+                    help="1st-order rate constant for the re-distribution.",
+                )
+                distributes.append([comp_i, comp_j, k_dist, k_redist])
+            left.write(" ")
+            left.write(" ")
+            # left.write(" ")
+else:
+    st.write("Only one compartment, so no distribution available.")
+
+st.write(" ")
+st.markdown("------")
+
+st.markdown("### 5. Drug Elimination")
+
+eliminates = []
+
+st.write(drug_name, " linear elimination from compartments:")
+
+for i in range(n_comp):
+    left, center, right = st.columns(3)
+    comp_i = compartments[i]["name"]
+    # left.write(" ")
+    is_on = left.toggle("{}".format(comp_i))
+    if is_on:
+        k_el = center.number_input(
+            "Elimination Rate constant:",
+            0.0,
+            help="1st-order rate constant for the linear elimination process.",
+            key="elimination_rate_{}".format(comp_i),
+        )
+        # right.markdown("------")
+        eliminates.append([comp_i, k_el])
+    # left.write(" ")
+    # left.write(" ")
+    # left.markdown("------")
+    # left.write(" ")
+    if n_comp > 1:
+        st.markdown("------")
+
+# st.write(" ")
+# st.markdown("------")
+
+# st.write(distributes)
+# st.write(eliminates)
+# st.write(compartments)
+st.markdown("### 6. Download Your PK Model")
 crafted = False
-if st.button("Craft it!"):
-    # pysb.Model()
-    # pkpd.two_compartments()
-    # drug = pkpd.drug_monomer(drug_name)[0]
-    # dosing_strategies[dosing](drug, CENTRAL, drug_dose, **dosing_kwargs)
-    # st.write(model)
-    # from pysb.export import export
-    model_file_name = model_name + ".py"
-    with open(model_file_name, 'w') as f:
+model_text = ""
+model_file_name = model_name + ".py"
+
+
+def write_model():
+    with open(model_file_name, "w") as f:
         writing.standard_imports(f)
-    crafted = True    
-
-
-if crafted:
-    with open(model_file_name, 'r') as f:
+        writing.new_model(f)
+        writing.compartments(f, compartments)
+        writing.dose(f, drug_name, drug_dose, dosing, dosing_kwargs)
+        if len(distributes) > 0:
+            writing.distribution(f, drug_name, distributes)
+        if len(eliminates) > 0:
+            writing.elimination(f, drug_name, eliminates)
+    crafted = True
+    with open(model_file_name, "r") as f:
         model_text = f.read()
-    st.write(model_text)
-    st.download_button("Download model", model_text, file_name=model_file_name)
+    return crafted, model_text
+
+
+# left, right = st.columns(2)
+# if left.button("Generate the Model"):
+#     crafted, model_text = write_model()
+#     st.code(model_text, line_numbers=True)
+#     right.download_button(
+#         "Download the model source code",
+#         model_text,
+#         model_file_name,
+#     )
+crafted, model_text = write_model()
+st.code(model_text, line_numbers=True)
+st.download_button(
+    'Download "{}"'.format(model_file_name),
+    model_text,
+    model_file_name,
+    on_click=st.balloons,
+)
+
+st.write(" ")
+st.write(" ")
+powered_by = "Powered by PySB {} and pysb-pkpd {}".format(
+    version("pysb"), version("pysb.pkpd")
+)
+st.caption(powered_by)
+# st.write(crafted, model_text)
+# if left.button("Craft it!"):
+#     crafted = write_model()
+
+# if crafted:
+
+
+# st.write(model_text)
+
+
+# editor_on = st.toggle("Turn on Code Editor")
+# if editor_on:
+#     with open(model_file_name, "r") as f:
+#         model_text = f.read()
+#     from streamlit_monaco import st_monaco
+#     content = st_monaco(value=model_text, height="600px", language="python")
--- pkcrafter/writing.py
+++ pkcrafter/writing.py
@@ -1,12 +1,78 @@
-
 dosing_strategies = {
-    "I.V. Bolus": 'pkpd.dose_bolus',
-    "I.V. Infusion": 'pkpd.dose_infusion',
-    "Oral": 'pkpd.dose_absorbed',
+    "I.V. Bolus": "pkpd.dose_bolus",
+    "I.V. Infusion": "pkpd.dose_infusion",
+    "Oral": "pkpd.dose_absorbed",
 }
+
 
 def standard_imports(file):
     file.write("from pysb import *\n")
     file.write("import pysb.pkpd as pkpd\n")
     file.write("\n")
     return
+
+def new_model(file):
+    file.write("##  Initialize the Model ## \n")
+    file.write("Model()\n")
+    file.write("\n")
+    return
+
+def compartments(file, comp_list):
+    file.write("##  Compartments  ##\n")
+    n_comp = len(comp_list)
+    if n_comp == 1:
+        file.write(
+            "pkpd.one_compartment(\"{}\", {}) \n".format(
+                comp_list[0]["name"], comp_list[0]["size"]
+            )
+        )
+    elif n_comp == 2:
+        file.write(
+            "pkpd.two_compartments(\"{}\", {}, \"{}\", {}) \n".format(
+                comp_list[0]["name"], comp_list[0]["size"],
+                comp_list[1]['name'], comp_list[1]["size"]
+            )
+        )
+    elif n_comp == 3:
+        file.write(
+            "pkpd.three_compartments(\"{}\", {}, \"{}\", {}, \"{}\", {}) \n".format(
+                comp_list[0]["name"], comp_list[0]["size"],
+                comp_list[1]['name'], comp_list[1]["size"],
+                comp_list[2]['name'], comp_list[2]["size"],
+            )
+        )
+    elif n_comp > 3:
+        for i in range(n_comp):
+            file.write("# Compartment {}".format(i+1))
+            file.write(
+                "pkpd.one_compartment(\"{}\", {}) \n".format(
+                    comp_list[i]["name"], comp_list[i]["size"]
+                )
+            )
+    else:
+        pass
+    file.write("\n")
+    return
+
+def dose(file, drug_name, drug_dose, dosing, dosing_kwargs):
+    file.write("##  Drug & Dose  ##\n")
+    file.write("pkpd.drug_monomer(\"{}\") \n".format(drug_name))
+    dose_line = "{}({}, {}".format(dosing_strategies[dosing], drug_name, drug_dose)
+    for item in dosing_kwargs.items():
+        dose_line += ", {}={}".format(*item)
+    dose_line += ") \n"
+    file.write(dose_line)
+    file.write("\n")
+    return
+
+def distribution(file, drug_name, distributions):
+    file.write("##  Drug Distribution  ## \n")
+    for dist in distributions:
+        file.write("pkpd.distribute({}, {}, {}, klist=[{}, {}]) \n".format(drug_name, *dist))
+    file.write("\n")
+
+def elimination(file, drug_name, eliminations):
+    file.write("##  Drug Elimination  ## \n")
+    for elim in eliminations:
+        file.write("pkpd.eliminate({}, {}, {}) \n".format(drug_name, *elim))
+    file.write("\n")
