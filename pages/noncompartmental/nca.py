8eac029d27af93ad152eff0cab93509c32952a97
blakeaw <blakeaw1102@gmail.com>
2024-09-06 13:56:43 -0500

    Moved the page descriptions for the NCA and Exposure-Response pages under expanders using new page_description widget.

--- pages/noncompartmental/nca.py
+++ pages/noncompartmental/nca.py
@@ -3,15 +3,14 @@ from app_util import util, widgets
 
 st.title("Non-Compartmental Analysis (NCA)")
 widgets.divider_blank()
-st.write(
-'''
+page_description = '''
 Welcome to the NCA page! Here, you can upload your
 concentration-time profiles and perform non-compartmental
 pharmacokinetic (PK) analysis on the data. Use this feature to
 calculate key PK parameters such as Cmax, Tmax, AUC, and half-life, without 
 assuming any specific compartmental model.
 '''
-)
+widgets.about_page(page_description)
 widgets.divider_blank()
 #widgets.under_construction()
 
--- pages/pdanalysis/exposureresponse.py
+++ pages/pdanalysis/exposureresponse.py
@@ -54,14 +54,13 @@ if "prm_functions_categorized" not in st.session_state:
     st.session_state.prm_functions_by_name = prm_functions_by_name(prm_functions)
 st.title("Fit Your Exposure-Response Data")
 widgets.divider_blank()
-st.write(
-    """
+page_description = """
 This tool lets you fit your experimental data with pharmacodynamic (PD) exposure-response models.
 This feature allows you to analyze how different drugs affect biological systems by fitting
 exposure-response data and other relevant parameters to established PD models. Use this tool to gain
 insights into the efficacy and potency of your compounds and optimize your drug development process.
 """
-)
+widgets.about_page(page_description)
 widgets.divider_blank()
 
 st.markdown("### 1. Upload Data")
